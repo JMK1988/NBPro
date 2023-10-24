74b44ab955a9940bcb88aea881baf15d4826fa19
jmk1988 <juanmaster23@gmail.com>
2023-10-24 09:24:06 -0300

    24/10
    ajustes varios, eliminacion de titulo seguro, cambio de logo mercantil andina

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export const Header = () => {
     <div className="container mx-auto flex items-center justify-center py-5 ">
     <img className="max-w-xs my-3" src="/Copia de NBP bordo.JPG"></img>
     </div>
-    <div className="container items-center mx-auto">
-    <h1 className="text-center font-bold m-0 mb-5">Formularios de Altas</h1>
+    <div className="container mx-auto">
+    <h1 className="text-center text-4xl mb-5 font-semibold">Formularios de Altas</h1>
     </div>
     
     </>
