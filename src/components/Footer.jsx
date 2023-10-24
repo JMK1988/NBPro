8d428023c8e0ae67e7d90a06a23d3dac091a74e6
jmk1988 <juanmaster23@gmail.com>
2023-10-24 10:05:24 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <>
             <div className="flex justify-center">
                 <div className="my-2">
-                    <p className="font-medium">NBProd© 2023 - Todos los Derechos Reservados</p>
+                    <p className="font-medium">NBPro© 2023 - Todos los Derechos Reservados</p>
                 </div>
             </div>
         </>
