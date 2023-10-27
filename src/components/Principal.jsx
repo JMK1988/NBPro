1f821302a4b3ead8b298092da65a5136f9fdc2a2
jmk1988 <juanmaster23@gmail.com>
2023-10-27 16:14:47 -0300

    refact

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -5,7 +5,6 @@ import { ModalProvider } from "./ModalProvider";
 import datosArchivos from '../../Scripts/datosArchivos.json';
 
 export const Principal = () => {
-  console.log(datosArchivos)
   const datos = datosArchivos;
   return (
     <ModalProvider>
@@ -14,39 +13,6 @@ export const Principal = () => {
           {Object.keys(datos).map((nombre, index) =>(
             <Card key={index} nombre={nombre} imagenes={datos[nombre]} />
           ))}
-
-
-          {/* <Card nombre="Allianz" />
-          <Card nombre="Asociart"/>
-          <Card nombre="ATM"/>
-          <Card nombre="Berkley"/>
-          <Card nombre="Cauciones"/>
-          <Card nombre="Chubb" />
-          <Card nombre="Colon"/>
-          <Card nombre="Experta ART"/>
-          <Card nombre="Federación"/>
-          <Card nombre="Galeno ART"/>
-          <Card nombre="Hanseatica" />
-          <Card nombre="HDI"/>
-          <Card nombre="La Holando"/>
-          <Card nombre="Libra"/>
-          <Card nombre="Mapfre"/>
-          <Card nombre="Mercantil" />
-          <Card nombre="Meridional"/>
-          <Card nombre="Noble"/>
-          <Card nombre="Pacifico"/>
-          <Card nombre="QBE ART" />
-          <Card nombre="RIO URUGUAY"/>
-          <Card nombre="Rivadavia"/>
-          <Card nombre="Sancor"/>
-          <Card nombre="Prevencion"/>
-          <Card nombre="SMG"/>
-          <Card nombre="SMG Life"/>
-          <Card nombre="Sura" />
-          <Card nombre="Testimonio"/>
-          <Card nombre="TPC"/>
-          <Card nombre="Victoria"/>
-          <Card nombre="Zurich"/> */}
         </div>
       </div>
     </ModalProvider>
