772ac5d558a923f42abd078f7c2e6881e2b310a2
JMK1988 <87148345+JMK1988@users.noreply.github.com>
2023-11-11 17:24:32 -0300

    accesi

--- src/components/ContactoAseguradoras.jsx
+++ src/components/ContactoAseguradoras.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import datosContactos from '../../Scripts/datosContactos.json';
 
 const ContactoAseguradoras = () => {
@@ -18,24 +18,58 @@ const ContactoAseguradoras = () => {
         setShowModal(false);
     };
 
+    const handleContact = (type, value) => {
+        switch (type) {
+            case 'WhatsApp':
+                window.open(`https://wa.me/${value}`, '_blank');
+                break;
+            case 'telefono':
+                window.open(`tel:${value}`, '_blank');
+                break;
+            case 'email':
+                window.open(`mailto:${value}`, '_blank');
+                break;
+            case 'web':
+                window.open(value, '_blank');
+                break;
+            case 'remolque':
+            case 'alarma':
+            case 'telefono_fijo':
+            case 'denuncia_de_siniestros_automotores':
+            case 'desde_exterior':
+                if (value.match(/\d+/g)) {
+                    window.open(`tel:${value}`, '_blank');
+                } else {
+                    console.log('Valor no válido para realizar llamada');
+                }
+                break;
+                case 'sms':
+                    if (value) {
+                        window.alert(`Mensaje: ${value}`);
+                    } else {
+                        console.log('No hay mensaje disponible para mostrar');
+                    }
+                    break;
+            default:
+                console.log('Tipo de contacto no compatible');
+        }
+    };
+
     return (
         <>
-            <div className="container mx-auto flex items-center justify-center py-5 ">
-                <img className="max-w-xs my-3" src="/Copia de NBP bordo.JPG"></img>
-            </div>
-            <div className="container mx-auto">
-                <h1 className="text-center text-4xl mb-5 font-semibold">Contactos Aseguradoras</h1>
-            </div>
             {showModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-10 max-h-screen" style={{ overflowY: "scroll" }}>
                     <div className="bg-white p-5 rounded-xl flex flex-col justify-center items-center gap-3 tarjeta">
                         {selectedContact && (
                             <>
-                                {Object.keys(selectedContact.contactos).map((contacto, index) => (
-                                    <p key={index}>
-                                        <b>{contacto}: </b>
-                                        {selectedContact.contactos[contacto]}
-                                    </p>
+                                {Object.entries(selectedContact.contactos).map(([contactType, contactValue], index) => (
+                                    <button
+                                        key={index}
+                                        className="text-center bg-gray-600 text-white py-1 px-6 rounded-lg font font-semibold hover:bg-gray-300 focus:scale-95 transition-all duration-200 ease-out w-full"
+                                        onClick={() => handleContact(contactType, contactValue)}
+                                    >
+                                        {contactType}
+                                    </button>
                                 ))}
                             </>
                         )}
@@ -49,36 +83,29 @@ const ContactoAseguradoras = () => {
                 </div>
             )}
 
-            <div className='m-5'>
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
-                    {datosContactos.companias.map((compania, index) => (
-
-                        <div
-                            key={index}
-                            className="bg-white p-5 rounded-xl shadow-lg  h-72 flex flex-col justify-between items-center gap-3 tarjeta"
-                            onClick={() => openModal(compania)}
-                        >
-                            <div className="flex justify-center  rounded-xl overflow-hidden flex-grow">
-                                <img src={`/${compania.nombre}.png`} alt={compania.nombre} className="object-contain no-select" />
-
-                            </div>
-                            <div className="flex justify-between flex-col w-full items-center">
-                                <button
-                                    className="text-center bg-gray-600 text-white py-1 px-6 rounded-lg font font-semibold hover:bg-gray-300 focus:scale-95 transition-all duration-200 ease-out w-full"
-                                >
-                                    Contacto
-                                </button>
-                            </div>
-                            {/* Aquí podrías incluir cualquier otro detalle que desees mostrar */}
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
+                {datosContactos.companias.map((compania, index) => (
+                    <div
+                        key={index}
+                        className="bg-white p-5 rounded-xl shadow-lg h-72 flex flex-col justify-between items-center gap-3 tarjeta"
+                        onClick={() => openModal(compania)}
+                    >
+                        <div className="flex justify-center rounded-xl overflow-hidden flex-grow">
+                            <img src={`/${compania.nombre}.png`} alt={compania.nombre} className="object-contain no-select" />
                         </div>
-                    ))}
-                </div>
+                        <div className="flex justify-between flex-col w-full items-center">
+                            <button
+                                className="text-center bg-gray-600 text-white py-1 px-6 rounded-lg font font-semibold hover:bg-gray-300 focus:scale-95 transition-all duration-200 ease-out w-full"
+                            >
+                                Contacto
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-
-
-
         </>
     );
-}
+};
 
 export default ContactoAseguradoras;
+
