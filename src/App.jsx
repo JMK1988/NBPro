41bac199e64595852003e1f99841cec98c794d84
JMK1988 <87148345+JMK1988@users.noreply.github.com>
2023-12-27 16:26:10 -0300

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import  Home  from "./components/Home";
-import Card4 from "./components/Card4";
+{/*import Card4 from "./components/Card4";*/}
 import ContactoAseguradoras from "./components/ContactoAseguradoras";
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/nbpro' element={<Card4></Card4>}></Route>
-      <Route path='/contactoAseguradoras' element={<ContactoAseguradoras></ContactoAseguradoras>} ></Route>
+      <Route path='/altas' element={<Home></Home>}></Route>
+      {/*<Route path='/nbpro' element={<Card4></Card4>}></Route>*/}
+      <Route path='/asistencia' element={<ContactoAseguradoras></ContactoAseguradoras>} ></Route>
       <Route path='/*' element={<Navigate to='/'/>}></Route>
     </Routes>
 
